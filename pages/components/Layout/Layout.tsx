5c2148846c645c0c861d6a3493db01e3cd767d70
QuocNha <quocnha@example.com>
2021-01-15 12:50:17 +0700

    A

--- pages/components/Layout/Layout.tsx
+++ pages/components/Layout/Layout.tsx
@@ -6,37 +6,39 @@ import MenuBar from '../MenuOrder/MenuBar';
 import React, { useEffect,useState } from 'react';
 import SearchInput from '../SearchInput/SearchInput';
 import FoodBody from './FoodBody/FoodBody';
-import Main from './Main/Main';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import {Button,Drawer,Toolbar,List,ListItem,ListItemText,ListItemIcon,Divider,AppBar} from '@material-ui/core';
 import { fade, makeStyles } from '@material-ui/core';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
-const drawerWidth=240;
-
-const useStyles = makeStyles((theme) => ({
-    appBar:{
-        zIndex: theme.zIndex.drawer + 1,
-    },
-    drawer: {
-        width: drawerWidth,
-        flexShrink: 0,
-    },
-    drawerContainer: {
-        overflow: 'auto',
-      },
-}));
+import CssBaseline from '@material-ui/core/CssBaseline';
 const Layout = ({}) => {
-    const classes = useStyles();
-
+   
+    const drawerWidth=240;
+    const useStyles = makeStyles((theme) => ({
+        appBar:{
+            zIndex: theme.zIndex.drawer + 1,
+        },
+        drawer: {
+            width: drawerWidth,
+            flexShrink: 0,
+        },
+        drawerContainer: {
+            overflow: 'auto',
+          },
+    }));
+    
+        const classes = useStyles();
+   
    return <div className={styles.container} >
             <Head>
             <title>Phong Vũ</title>
             <link rel="icon" href="/favicon.ico" />
             <meta name="viewport" content="initial-scale=1.0, width=device-width" />
             </Head>
-            <CssBaseline />
-            <MenuBar></MenuBar>
+            <React.Fragment>
+        <AppBar position="fixed" className={classes.appBar}>
+        <MenuBar></MenuBar>
+        </AppBar>;
             <Drawer
                 className={classes.drawer}
                 variant="permanent"
@@ -67,19 +69,22 @@ const Layout = ({}) => {
                     </List>
                 </div>
             </Drawer>
-        <main className={styles.main} >
-          
+            </React.Fragment>
+        {/* <main className={styles.main} >
+           <div className={styles.Menu}>
+                <MenuBar></MenuBar>
+           </div>
            <div className={styles.Search}>
-            {/* <SearchInput></SearchInput> */}
+            <SearchInput></SearchInput>
            </div>
            
            <div className={styles.body}>
               <FoodBody></FoodBody>
            </div>
-           {/* <div className={styles.MenuOrder}>
+           <div className={styles.MenuOrder}>
                <MenuOrder ></MenuOrder>
-           </div> */}
-        </main>
+           </div>
+        </main> */}
 
         {/* <footer className={styles.footer}>
             footer
--- pages/components/Layout/Main/Main.tsx
+++ pages/components/Layout/Main/Main.tsx
@@ -22,11 +22,39 @@ const useStyles = makeStyles((theme) => ({
 const Main = () => {
     const classes = useStyles();
     return <React.Fragment>
-        <CssBaseline/>
-    <AppBar position="fixed" className={classes.appBar}>
+        <AppBar position="fixed" className={classes.appBar}>
         <MenuBar></MenuBar>
-    </AppBar>;
-            
+        </AppBar>;
+            <Drawer
+                className={classes.drawer}
+                variant="permanent"
+                classes={{
+                    // paper: classes.drawerPaper,
+                }}
+            >
+                <Toolbar />
+                <div 
+                 className={classes.drawerContainer}
+                >
+                    <List>
+                        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                            <ListItem button key={text}>
+                                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                                <ListItemText primary={text} />
+                            </ListItem>
+                        ))}
+                    </List>
+                    <Divider />
+                    <List>
+                        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                            <ListItem button key={text}>
+                                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                                <ListItemText primary={text} />
+                            </ListItem>
+                        ))}
+                    </List>
+                </div>
+            </Drawer>
     </React.Fragment>
 }
 export default Main;
