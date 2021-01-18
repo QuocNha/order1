198f547d96c16e850e695cd5a1b515eaf4dcce74
QuocNha <quocnha@example.com>
2021-01-18 11:22:31 +0700

    error render server side

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import React, { Fragment, useEffect } from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Link from 'next/link';
+import { LayersClearOutlined } from '@material-ui/icons';
+import Layout from './components/Layout/Layout';
 
 // import styles from '../styles/Home.module.scss'
 
@@ -18,7 +20,8 @@ const Home = () => {
   return (
     <Fragment>
       <div id="demo" >
-      <Link  href="/components/Layout/Layout"><a >Vui lòng click để vào trang  web</a></Link>
+      {/* <Link  href="/components/Layout/Layout"><a >Vui lòng click để vào trang  web</a></Link> */}
+      <Layout></Layout>
       </div>
     
     </Fragment>
