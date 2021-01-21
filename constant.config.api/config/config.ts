8fbedf16cc5441547baf377006cd2146dbf23cf8
QuocNha <quocnha@example.com>
2021-01-21 16:49:53 +0700

    custom URL

---
+++ constant.config.api/config/config.ts
@@ -0,0 +1,8 @@
+//const SERVERAPI = "http://localhost:3000/";
+// bang3 LIve
+       const SERVERAPI ="https://order-demo-nextjs.herokuapp.com/";
+
+const RESIGN = SERVERAPI + "api/resign";
+export {
+    RESIGN
+}
--- constant.config.api/getAllUser.ts
+++ constant.config.api/getAllUser.ts
@@ -1,9 +1,13 @@
 import axios from 'axios';
+const resign =  "http://localhost:3000/api/resign";
+//server
+//const resign =  "https://order-demo-nextjs.herokuapp.com/api/resign";
 
+import {RESIGN} from '../constant.config.api/config/config'
 async function getAllUser() {
     let data;
     await axios.get(
-        "https://order-demo-nextjs.herokuapp.com/api/resign",
+        RESIGN,
         
     )
         .then(function (response) {
